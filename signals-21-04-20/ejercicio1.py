7404921eda80385551060509ca12b92eaa73527e
franco.alfano.95@gmail.com <franco.alfano.95@gmail.com>
2020-04-24 17:36:22 -0300

    adding signals folder

---
+++ signals-21-04-20/ejercicio1.py
@@ -0,0 +1,23 @@
+#Ejercicio 1
+
+import signal
+import os
+import time
+
+def receiveSignal(signalNumber, frame):
+    print('Received:', signalNumber)
+    raise SystemExit('Exiting')
+    return
+
+if __name__ == '__main__':
+    # register the signal to be caught
+    signal.signal(signal.SIGUSR1, receiveSignal)
+
+    # register the signal to be ignored
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    # output current process id
+    print('My PID is:', os.getpid())
+
+    signal.pause()
+
